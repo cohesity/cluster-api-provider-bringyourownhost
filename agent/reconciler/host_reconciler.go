876e1b250cc4ca1c3891d0a2b6f8f8877dec1afe
avaneesh.reddy <avaneesh.reddy@cohesity.com>
2025-12-10 20:33:26 -0800

    remove containers before running kubeadm reset

--- agent/reconciler/host_reconciler.go
+++ agent/reconciler/host_reconciler.go
@@ -16,6 +16,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
+	utilsexec "k8s.io/utils/exec"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/util/conditions"
 	"sigs.k8s.io/cluster-api/util/patch"
@@ -25,6 +26,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	infrastructurev1beta1 "github.com/cohesity/cluster-api-provider-bringyourownhost/api/infrastructure/v1beta1"
+	byohutil "github.com/cohesity/cluster-api-provider-bringyourownhost/util"
 	"github.com/kube-vip/kube-vip/pkg/vip"
 )
 
@@ -43,6 +45,11 @@ const (
 	bootstrapSentinelFile = "/run/cluster-api/bootstrap-success.complete"
 	// KubeadmResetCommand is the command to run to force reset/remove nodes' local file system of the files created by kubeadm
 	KubeadmResetCommand = "kubeadm reset --force"
+)
+
+var (
+	// errContainerRuntimeNil is returned when container runtime is nil
+	errContainerRuntimeNil = errors.New("container runtime is nil")
 )
 
 // Reconcile handles events for the ByoHost that is registered by this agent process
@@ -300,9 +307,19 @@ func (r *HostReconciler) hostCleanUp(ctx context.Context, byoHost *infrastructur
 
 func (r *HostReconciler) resetNode(ctx context.Context, byoHost *infrastructurev1beta1.ByoHost) error {
 	logger := ctrl.LoggerFrom(ctx)
+
+	logger.Info("Removing containers using crictl")
+	err := r.removeContainers(ctx)
+	if err != nil {
+		r.Recorder.Event(byoHost, corev1.EventTypeWarning, "RemoveContainersFailed", "failed to remove containers using crictl")
+		return errors.Wrapf(err, "failed to remove containers using crictl")
+	}
+
+	logger.Info("Removed containers successfully")
+
 	logger.Info("Running kubeadm reset")
 
-	err := r.CmdRunner.RunCmd(ctx, KubeadmResetCommand)
+	err = r.CmdRunner.RunCmd(ctx, KubeadmResetCommand)
 	if err != nil {
 		r.Recorder.Event(byoHost, corev1.EventTypeWarning, "ResetK8sNodeFailed", "k8s Node Reset failed")
 		return errors.Wrapf(err, "failed to exec kubeadm reset")
@@ -322,6 +339,39 @@ func (r *HostReconciler) bootstrapK8sNode(ctx context.Context, bootstrapScript s
 	}.Execute(bootstrapScript)
 	if err != nil {
 		return fmt.Errorf("failed to execute bootstrap script: %w", err)
+	}
+	return nil
+}
+
+func (r *HostReconciler) removeContainers(ctx context.Context) error {
+	// Cleanup kubelet before removing containers
+	err := byohutil.CleanupKubelet(ctx, r.CmdRunner)
+	if err != nil {
+		return errors.Wrapf(err, "failed to cleanup kubelet")
+	}
+
+	criSocketPath, err := byohutil.DetectCRISocket()
+	if err != nil {
+		return errors.Wrapf(err, "failed to detect cri socket path")
+	}
+
+	containerRuntime, err := byohutil.NewContainerRuntime(utilsexec.New(), criSocketPath)
+	if err != nil {
+		return errors.Wrapf(err, "failed to create container runtime")
+	}
+
+	if containerRuntime == nil {
+		return errors.Wrapf(errContainerRuntimeNil, "container runtime is nil after initialization")
+	}
+
+	containers, err := containerRuntime.ListContainers()
+	if err != nil {
+		return errors.Wrapf(err, "failed to list containers")
+	}
+
+	err = containerRuntime.RemoveContainers(ctx, containers)
+	if err != nil {
+		return errors.Wrapf(err, "failed to remove containers")
 	}
 	return nil
 }
--- util/util.go
+++ util/util.go
@@ -8,17 +8,37 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	errorsutil "k8s.io/apimachinery/pkg/util/errors"
+	utilsexec "k8s.io/utils/exec"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	"github.com/cohesity/cluster-api-provider-bringyourownhost/agent/cloudinit"
 	infrastructurev1beta1 "github.com/cohesity/cluster-api-provider-bringyourownhost/api/infrastructure/v1beta1"
 )
 
-// ByoMachineKind is the Kind for ByoMachine
-const ByoMachineKind = "ByoMachine"
+const (
+	// ByoMachineKind is the Kind for ByoMachine
+	ByoMachineKind = "ByoMachine"
+
+	RemoveContainerRetry = 5
+
+	// SocketDialTimeout is the timeout for checking socket connectivity
+	SocketDialTimeout = 5 * time.Second
+
+	// Kubelet cleanup commands
+	RemoveKubeletBinaryCmd = "rm -f /usr/bin/kubelet"
+	StopKubeletServiceCmd  = "systemctl stop kubelet || true"
+)
 
 var (
 	// ErrMachineRefNotSet is returned when machineRef is not set in ByoHost status
@@ -29,7 +49,32 @@ var (
 
 	// ErrMachineRefIncorrectGroup is returned when machineRef has incorrect group
 	ErrMachineRefIncorrectGroup = errors.New("machineRef has incorrect group")
+
+	// ErrMultipleCRIEndpoints is returned when multiple CRI endpoints are found
+	ErrMultipleCRIEndpoints = errors.New("found multiple CRI endpoints on the host")
+
+	// defaultKnownCRISockets holds the set of known CRI endpoints
+	defaultKnownCRISockets = []string{
+		"unix:///var/run/containerd/containerd.sock",
+		"unix:///var/run/crio/crio.sock",
+		"unix:///var/run/docker.sock",
+	}
+
+	DefaultCRISocket = "unix:///var/run/containerd/containerd.sock"
 )
+
+type ContainerRuntime interface {
+	Socket() string
+	ListContainers() ([]string, error)
+	RemoveContainers(ctx context.Context, containers []string) error
+}
+
+// CRIRuntime is a struct that interfaces with the CRI
+type CRIRuntime struct {
+	exec       utilsexec.Interface
+	criSocket  string
+	crictlPath string
+}
 
 // GetOwnerByoMachine returns the ByoMachine object owning the current resource.
 func GetOwnerByoMachine(ctx context.Context, c client.Client, obj *metav1.ObjectMeta) (*infrastructurev1beta1.ByoMachine, error) {
@@ -99,4 +144,138 @@ func GetByoMachineForHost(ctx context.Context, c client.Client, byoHost *infrast
 	}
 
 	return byoMachine, nil
+}
+
+// detectCRISocketImpl is separated out only for test purposes, DON'T call it directly, use DetectCRISocket instead
+func detectCRISocketImpl(isSocket func(string) bool, knownCRISockets []string) (string, error) {
+	foundCRISockets := []string{}
+
+	for _, socket := range knownCRISockets {
+		if isSocket(socket) {
+			foundCRISockets = append(foundCRISockets, socket)
+		}
+	}
+
+	switch len(foundCRISockets) {
+	case 0:
+		// Fall back to the default socket if no CRI is detected, we can error out later on if we need it
+		return DefaultCRISocket, nil
+	case 1:
+		// Precisely one CRI found, use that
+		return foundCRISockets[0], nil
+	default:
+		// Multiple CRIs installed?
+		return "", fmt.Errorf("%w: please define which one to use by setting the 'criSocket' field in the kubeadm configuration file: %s",
+			ErrMultipleCRIEndpoints, strings.Join(foundCRISockets, ", "))
+	}
+}
+
+// DetectCRISocket uses a list of known CRI sockets to detect one. If more than one or none is discovered, an error is returned.
+func DetectCRISocket() (string, error) {
+	return detectCRISocketImpl(isExistingSocket, defaultKnownCRISockets)
+}
+
+// isExistingSocket checks if path exists and is domain socket
+func isExistingSocket(path string) bool {
+	u, err := url.Parse(path)
+	if err != nil {
+		// should not happen, since we are trying to access known / hardcoded sockets
+		return false
+	}
+
+	// Create a context with timeout for the dial operation
+	ctx, cancel := context.WithTimeout(context.Background(), SocketDialTimeout)
+	defer cancel()
+
+	dialer := &net.Dialer{}
+	c, err := dialer.DialContext(ctx, u.Scheme, u.Path)
+	if err != nil {
+		return false
+	}
+	_ = c.Close()
+	return true
+}
+
+// NewContainerRuntime sets up and returns a ContainerRuntime struct
+func NewContainerRuntime(execer utilsexec.Interface, criSocket string) (ContainerRuntime, error) {
+	const toolName = "crictl"
+	crictlPath, err := execer.LookPath(toolName)
+	if err != nil {
+		return nil, fmt.Errorf("%s is required by the container runtime: %w", toolName, err)
+	}
+	return &CRIRuntime{execer, criSocket, crictlPath}, nil
+}
+
+// ListContainers lists running k8s CRI pods
+func (runtime *CRIRuntime) ListContainers() ([]string, error) {
+	// Disable debug mode regardless how the crictl is configured so that the debug info won't be
+	// iterpreted to the Pod ID.
+	args := []string{"-D=false", "pods", "-q"}
+	out, err := runtime.crictl(args...).CombinedOutput()
+	if err != nil {
+		return nil, fmt.Errorf("output: %s, error: %w", string(out), err)
+	}
+	pods := []string{}
+	pods = append(pods, strings.Fields(string(out))...)
+	return pods, nil
+}
+
+// crictl creates a crictl command for the provided args.
+func (runtime *CRIRuntime) crictl(args ...string) utilsexec.Cmd {
+	cmd := runtime.exec.Command(runtime.crictlPath, append([]string{"-r", runtime.Socket(), "-i", runtime.Socket()}, args...)...)
+	cmd.SetEnv(os.Environ())
+	return cmd
+}
+
+// Socket returns the CRI socket endpoint
+func (runtime *CRIRuntime) Socket() string {
+	return runtime.criSocket
+}
+
+// RemoveContainers removes running k8s pods
+func (runtime *CRIRuntime) RemoveContainers(ctx context.Context, containers []string) error {
+	logger := ctrl.LoggerFrom(ctx)
+	errs := []error{}
+	for _, container := range containers {
+		var lastErr error
+		for i := 0; i < RemoveContainerRetry; i++ {
+			logger.V(5).Info("Attempting to remove container", "container", container)
+			out, err := runtime.crictl("stopp", container).CombinedOutput()
+			if err != nil {
+				lastErr = fmt.Errorf("failed to stop running pod %s: output: %s: %w", container, string(out), err)
+				continue
+			}
+			out, err = runtime.crictl("rmp", container).CombinedOutput()
+			if err != nil {
+				lastErr = fmt.Errorf("failed to remove running container %s: output: %s: %w", container, string(out), err)
+				continue
+			}
+			lastErr = nil
+			break
+		}
+
+		if lastErr != nil {
+			errs = append(errs, lastErr)
+		}
+	}
+	return errorsutil.NewAggregate(errs)
+}
+
+// CleanupKubelet removes the kubelet binary and stops the kubelet service
+func CleanupKubelet(ctx context.Context, cmdRunner cloudinit.ICmdRunner) error {
+	logger := ctrl.LoggerFrom(ctx)
+
+	// First remove kubelet binary
+	logger.Info("Removing kubelet binary")
+	if err := cmdRunner.RunCmd(ctx, RemoveKubeletBinaryCmd); err != nil {
+		return fmt.Errorf("failed to remove kubelet binary: %w", err)
+	}
+
+	// Then stop kubelet service
+	logger.Info("Stopping kubelet service")
+	if err := cmdRunner.RunCmd(ctx, StopKubeletServiceCmd); err != nil {
+		return fmt.Errorf("failed to stop kubelet service: %w", err)
+	}
+
+	return nil
 }
